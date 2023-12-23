d96120975b82a6cb073ccc011da71305cb1c0bd3
ajroot5685 <anjeong8577@naver.com>
2023-12-23 23:31:03 +0900

    #3 feat: postSQL 연결 및 작동 확인

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -7,6 +7,37 @@ module.exports = {
             {id:"1", title:"test1", content:"content1"},
             {id:"2", title:"test2", content:"content2"}];
 
+        // db 테스트 코드
+        db.connect();
+        // db.query("SELECT NOW()", (err, res) => {
+        //     console.log(err, res);
+        //     db.end();
+        // });
+
+        // insert
+        let query = {
+            text: "INSERT INTO \"User\" (id, password, name, nickname) VALUES ($1, $2, $3, $4)",
+            values: ["id1", "pwd1", "name1", "nickname1"],
+        };
+        db
+            .query(query)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((e) => console.error(e.stack));
+
+        // select
+        query = {
+            text: "SELECT * FROM \"User\"",
+        };
+        db
+            .query(query)
+            .then((res) => {
+                console.log(res.rows[0]);
+                db.end();
+            })
+            .catch((e) => console.error(e.stack));
+
         return testpost;
     },
 
