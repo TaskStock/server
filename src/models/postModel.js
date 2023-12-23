53d13c950c6a4a232a465a8c8d304a6e0922fe79
Seown Min <nvpz1598@gamil.com>
2023-12-24 05:19:26 +0900

    #develop: feat: postgreSQL 개발용 DB 작동 확인

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,44 +1,21 @@
 const db = require('../config/db.js');
 
 module.exports = {
-    // board_type_id 로 서로 다른 게시글 데이터를 가져옴
     getAll: async()=>{
-        testpost = [
-            {id:"1", title:"test1", content:"content1"},
-            {id:"2", title:"test2", content:"content2"}];
-
         // db 테스트 코드
         db.connect();
-        // db.query("SELECT NOW()", (err, res) => {
-        //     console.log(err, res);
-        //     db.end();
-        // });
+        
+        //insert
+        const text = "insert into test (email) values($1);"
+        const values = ['123456@gmail.com'];
 
-        // insert
-        let query = {
-            text: "INSERT INTO \"User\" (id, password, name, nickname) VALUES ($1, $2, $3, $4)",
-            values: ["id1", "pwd1", "name1", "nickname1"],
-        };
-        db
-            .query(query)
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((e) => console.error(e.stack));
+        db.query(text, values)
+            .then(res => console.log(res.rows[0]))
+            .catch(e => console.error(e.stack));
 
-        // select
-        query = {
-            text: "SELECT * FROM \"User\"",
-        };
-        db
-            .query(query)
-            .then((res) => {
-                console.log(res.rows[0]);
-                db.end();
-            })
-            .catch((e) => console.error(e.stack));
+        //select
+        const testPost = db.query("SELECT * FROM test")
+        return testPost
 
-        return testpost;
     },
-
 }
