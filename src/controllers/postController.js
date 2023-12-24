11b8e18f4f7c0e95d70307a2ef17c79144be3c89
Seown Min <nvpz1598@gamil.com>
2023-12-24 20:15:57 +0900

    #9 fix: post관련된 것 삭제

--- src/controllers/postController.js
+++
@@ -1,9 +0,0 @@
-const postModel = require('../models/postModel.js');
-
-module.exports = {
-    getAll: async(req, res) =>{
-        const posts = await postModel.getAll();
-        
-        res.json({posts: posts});
-    },
-}
--- src/routers/postRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const postController = require('../controllers/postController.js');
-
-router.get('/', postController.getAll);
-
-module.exports = router;
