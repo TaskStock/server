0b03f8f902784dd712ec1d25d34391b4c526d520
Seown Min <nvpz1598@gamil.com>
2023-12-26 02:43:57 +0900

    #11 fix: 인증코드 0으로 시작할 때 문제 해결 및 코드 리팩토링

--- src/models/accountModel.js
+++ src/models/accountModel.js
@@ -1,12 +1,32 @@
+/*
+{
+    command: 'SELECT',
+    rowCount: 3, // 반환된 행의 수
+    oid: null,   // 대부분의 경우 사용하지 않음
+    rows: [      // 실제 데이터를 포함하는 배열
+        { id: 1, username: 'user1', email: 'user1@example.com' },
+        { id: 2, username: 'user2', email: 'user2@example.com' },
+        { id: 3, username: 'user3', email: 'user3@example.com' }
+    ],
+    fields: [    // 반환된 각 열에 대한 정보를 포함하는 객체의 배열
+        { name: 'id', tableID: 12345, columnID: 1, dataTypeID: 23, ... },
+        { name: 'username', tableID: 12345, columnID: 2, dataTypeID: 1043, ... },
+        { name: 'email', tableID: 12345, columnID: 3, dataTypeID: 1043, ... }
+    // ...
+    ],
+  // ... 추가적인 내부 속성들이 있을 수 있음
+}
+*/
 const db = require('../config/db.js');
 
 module.exports = {
     checkAvailible: async(emailData) => {
         const query = 'SELECT user_id FROM "User" WHERE email = $1';
-        const email = [emailData.email];
+        const email = emailData.email;
         console.log(email);
-        const result = await db.query(query, email);
-        if(result.rowCount === 0){ // 가입된 이메일이 없을 경우
+
+        const {rowCount} = await db.query(query, [email]);
+        if(rowCount === 0){ // 가입된 이메일이 없을 경우
             return true;
         }else{  // 가입된 이메일이 있을 경우
             return false;
@@ -14,19 +34,19 @@ module.exports = {
     },
     saveCode: async(authCode) => {
         const query = 'INSERT INTO "Code" (auth_code) VALUES ($1) RETURNING code_id';
-        const code = [authCode];
-        const {rows} = await db.query(query, code); // 구조분해 할당
+        const code = authCode;
+        const {rows} = await db.query(query, [code]); // 구조분해 할당
         const codeId = rows[0].code_id; 
         console.log("코드 db에 저장 완료");
         return codeId 
     },
     checkCode: async(inputData) => {
         const query = 'SELECT auth_code FROM "Code" WHERE code_id = $1';
-        const codeId = [inputData.codeId];
-        const {rows} = await db.query(query, codeId);
+        const codeId = inputData.codeId;
+        const {rows} = await db.query(query, [codeId]);
 
-        const authCode = rows[0].auth_code; // 인증코드
-        const inputCode = inputData.inputCode; // 사용자가 입력한 코드
+        const authCode = rows[0].auth_code; // 인증코드(string)
+        const inputCode = inputData.inputCode; // 사용자가 입력한 코드(string)
 
         if (authCode == inputCode) {
             return true;
@@ -36,8 +56,8 @@ module.exports = {
     },
     deleteCode: async(inputData) => {
         const query = 'DELETE FROM "Code" WHERE code_id = $1';
-        const code = [inputData.codeId];
-        deleteResult = await db.query(query, code);
+        const code = inputData.codeId;
+        deleteResult = await db.query(query, [code]);
         
         if (deleteResult.rowCount === 1) {
             return true;
