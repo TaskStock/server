f168df5085279c22441f0a6e4a1b556161fbcc24
ajroot5685 <anjeong8577@naver.com>
2023-12-27 01:07:33 +0900

    #18 refact: 인자 받는 방식 수정

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -2,10 +2,11 @@ const todoModel = require('../models/todoModel.js');
 
 module.exports = {
     newTodo: async(req, res, next) =>{
-        const todoData = req.body;
+        const {title, content, level, user_id, project_id} = req.body;
+        // 현재는 오늘 날짜만 todo를 생성할 수 있음
         
         try{
-            await todoModel.insertTodo(todoData);
+            await todoModel.insertTodo(title, content, level, user_id, project_id);
             // 순서 관련 로직 필요
         }catch(error){
             next(error);
@@ -16,11 +17,12 @@ module.exports = {
     // 유저아이디와 날짜를 받아서 해당하는 todo들을 반환
     // 날짜 받을 때 지역정보?도 같이 받아야 utc 기준으로 계산 가능
     readTodo: async(req, res, next) =>{
-        const todoData = req.body;
+        const {user_id, date} = req.body;
+        // date : 클라이언트로부터 받은 시간 정보를 UTC 기준으로 변환한 후 년,월,일까지 저장하도록 처리할것 ex. 2023-12-24
         
         let todos;
         try{
-            todos = await todoModel.readTodo(todoData);
+            todos = await todoModel.readTodo(user_id, date);
         }catch(error){
             next(error);
         }
@@ -28,10 +30,10 @@ module.exports = {
         res.json({todos: todos});
     },
     updateTodo: async(req, res, next) =>{
-        const todoData = req.body;
+        const {todo_id, title, content, level, user_id, project_id} = req.body;
         
         try{
-            await todoModel.updateTodo(todoData);
+            await todoModel.updateTodo(todo_id, title, content, level, user_id, project_id);
         }catch(error){
             next(error);
         }
@@ -40,10 +42,10 @@ module.exports = {
     },
     // 유저 id와 todo id를 받아 해당 todo 삭제
     deleteTodo: async(req, res, next) =>{
-        const todoData = req.body;
+        const {todo_id, user_id} = req.body;
         
         try{
-            await todoModel.deleteTodo(todoData);
+            await todoModel.deleteTodo(todo_id, user_id);
         }catch(error){
             next(error);
         }
