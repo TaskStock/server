935aff85373a57b7cce4a20fe73457e55d6c631e
ajroot5685 <anjeong8577@naver.com>
2024-01-21 17:33:09 +0900

    #163 feat: 회고록 조회(프로젝트 단위, 전체조회)

--- src/routers/retrospectRouter.js
+++ src/routers/retrospectRouter.js
@@ -7,5 +7,7 @@ router.post('', retrospectController.newRetrospect);
 router.put('', retrospectController.updateRetrospect);
 router.delete('', retrospectController.deleteRetrospect);
 router.get('/month', retrospectController.getRetrospectsWithMonth);
+router.get('/all', retrospectController.getRetrospectsAll);
+router.get('/project/:project_id', retrospectController.getRetrospectsWithProject);
 
 module.exports = router;
