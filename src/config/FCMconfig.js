828bcffc1965a3a93a6b4d5e45f2fcf57a5d5c28
Seown Min <nvpz1598@gamil.com>
2024-01-30 00:44:31 +0900

    #188 feat: noticeService에 푸시알림 등록

--- src/config/FCMconfig.js
+++ src/config/FCMconfig.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const serviceAccount = require("../firebase-gcp-share-private-key.json");
+const serviceAccount = require("../../firebase-gcp-share-private-key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
