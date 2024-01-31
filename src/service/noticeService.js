292e9ac84a80c9257d4c105afc9d81b50d5a1dbe
Seown Min <nvpz1598@gamil.com>
2024-01-31 15:57:22 +0900

    #205 fix: noticeService 타입 수정 및 클릭 대상 target_id로 고정

--- src/service/noticeService.js
+++ src/service/noticeService.js
@@ -13,7 +13,7 @@ module.exports = {
             info: ''
         };
         
-        if (predata.type === 'sns.follow') {
+        if (predata.type === 'sns') {
             follower_name = await accountModel.getUserNameById(predata.follower_id);
             if (predata.pending === false)
                 noticeData.content = `${follower_name}님이 팔로우를 시작했습니다.`;
@@ -21,18 +21,18 @@ module.exports = {
                 noticeData.content = `${follower_name}님이 팔로우 요청을 보냈습니다.`;
             }
             noticeData.info = JSON.stringify({
-                follower_id: predata.follower_id,
+                target_id: predata.follower_id,
                 isFollowingMe: predata.isFollowingMe,
                 isFollowingYou: predata.isFollowingYou,
                 pending: predata.pending
             });
         }
 
-        if (predata.type === 'sns.accept') {
+        if (predata.type === 'general') {
             following_name = await accountModel.getUserNameById(predata.following_id);
             noticeData.content = `${following_name}님이 팔로우 요청을 수락했습니다.`;
             noticeData.info = JSON.stringify({
-                following_id: predata.following_id
+                target_id: predata.following_id
             });
         }
 
@@ -77,10 +77,16 @@ module.exports = {
             const user_name = await accountModel.getUserNameById(noticeData.user_id);
             const content = noticeData.content;
             message = `*${user_name}*님이 고객센터에 새로운 의견을 남겼습니다.\n\n${content}`;
+
+            await slackClient.chat.postMessage({
+                channel: '#고객의견',
+                text: message
+            })
+            return
         } 
-        await slackClient.chat.postMessage({
-            channel: '#고객의견',
-            text: message
-        })
-    }
+
+        if (noticeData.type === 'error') {
+            // const message
+        }
+    } 
 };
