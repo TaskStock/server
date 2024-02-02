e4318b287efc672095142043a4d3249523dbb4d4
ajroot5685 <anjeong8577@naver.com>
2024-02-02 17:29:29 +0900

    #211 feat: 통계에 대한 스케쥴러 작업

--- src/models/sistatisticsModel.js
+++ src/models/sistatisticsModel.js
@@ -14,5 +14,35 @@ module.exports = {
 
                 throw e;
             });
-    }
+    },
+    updateSistatistics: async(stockitem_id, total_count, success_count, dayOfWeek)=>{
+        // 0 : 일요일, 1 : 월요일, 2 : 화요일, 3 : 수요일, 4 : 목요일, 5 : 금요일, 6 : 토요일
+        let query;
+        if(dayOfWeek === 0){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, sunday=sunday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 1){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, monday=monday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 2){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, tuesday=tuesday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 3){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, wednesday=wednesday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 4){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, thursday=thursday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 5){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, friday=friday+$1 where stockitem_id=$3';
+        }else if(dayOfWeek === 6){
+            query = 'update "SIStatistics" set total_count=total_count+$1, success_count=success_count+$2, saturday=saturday+$1 where stockitem_id=$3';
+        }
+        const values = [total_count, success_count, stockitem_id];
+
+        await db.query(query, values)
+            .then(res => {
+                // console.log(res.rows[0]);
+            })
+            .catch(e => {
+                console.error(e.stack);
+
+                throw e;
+            });
+    },
 }
