0c339a9ddccbf059f0d567e158701397130d7ae6
ajroot5685 <anjeong8577@naver.com>
2024-02-05 20:59:31 +0900

    #251 feat: 시장탭 조회 api

--- src/routers/siuserRouter.js
+++ src/routers/siuserRouter.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const siuserController = require('../controllers/siuserController.js');
 
-router.get('/myinterest', siuserController.getItemsMyinterest);
-router.get('/todaypopular', siuserController.getItemsTodaypopular);
-router.get('/todayrecommend', siuserController.getItemsTodayrecommend);
+router.get('/market', siuserController.getMarketInfo);
 router.get('/all', siuserController.getItemsAll);
 router.get('/detail/:stockitem_id', siuserController.getItem);
 
