b1b9cf2735c13ee787e3f487b80852b56b280f19
ajroot5685 <anjeong8577@naver.com>
2024-02-06 15:58:23 +0900

    #246 feat: simap 식별자 제거, 로직 수정

--- src/models/simapModel.js
+++ src/models/simapModel.js
@@ -1,9 +1,9 @@
 module.exports = {
-    getSimapid: async(db, user_id, stockitem_id)=>{
-        const query = 'select simap_id from "SIMap" where user_id=$1 and stockitem_id=$2';
+    getSimap: async(db, user_id, stockitem_id)=>{
+        const query = 'select stockitem_id from "SIMap" where user_id=$1 and stockitem_id=$2';
         const values = [user_id, stockitem_id];
 
-        const simap_id = await db.query(query, values)
+        const simap = await db.query(query, values)
             .then(res => {
                 // console.log(res.rows[0]);
                 return res.rows[0];
@@ -13,7 +13,7 @@ module.exports = {
 
                 throw e;
             });
-        return simap_id;
+        return simap;
     },
     createSimap: async(db, user_id, stockitem_id)=>{
         const query = 'insert into "SIMap" (user_id, stockitem_id) VALUES ($1, $2)';
@@ -29,9 +29,9 @@ module.exports = {
                 throw e;
             });
     },
-    increaseTakecount: async(db, simap_id)=>{
-        const query = 'update "SIMap" set take_count=take_count+1 where simap_id=$1';
-        const values = [simap_id];
+    increaseTakecount: async(db, user_id, stockitem_id)=>{
+        const query = 'update "SIMap" set take_count=take_count+1 where user_id=$1 and stockitem_id=$2';
+        const values = [user_id, stockitem_id];
 
         await db.query(query, values)
             .then(res => {
@@ -43,9 +43,9 @@ module.exports = {
                 throw e;
             });
     },
-    decreaseTakecount: async(db, simap_id)=>{
-        const query = 'update "SIMap" set take_count=take_count-1 where simap_id=$1';
-        const values = [simap_id];
+    decreaseTakecount: async(db, user_id, stockitem_id)=>{
+        const query = 'update "SIMap" set take_count=take_count-1 where user_id=$1 and stockitem_id=$2';
+        const values = [user_id, stockitem_id];
 
         await db.query(query, values)
             .then(res => {
